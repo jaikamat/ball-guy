460a82d604965632f95d3fc9ac1346be578ab41f
Daniel Garlans <dgarlans@gmail.com>
2023-06-27 01:36:53 -0400

    added time code, and some initial state variables

--- code.py
+++ code.py
@@ -7,6 +7,7 @@ background, a smaller black rectangle, and some white text.
 """
 
 import board
+import supervisor
 import displayio
 import digitalio
 import terminalio
@@ -14,6 +15,20 @@ import neopixel
 from adafruit_display_text import label
 import adafruit_displayio_ssd1306
 
+# functions for timekeeping
+_TICKS_PERIOD = const(1<<29)
+_TICKS_MAX = const(_TICKS_PERIOD-1)
+_TICKS_HALFPERIOD = const(_TICKS_PERIOD//2)
+
+def ticks_add(ticks, delta):
+    "Add a delta to a base number of ticks, performing wraparound at 2**29ms."
+    return (ticks + delta) % _TICKS_PERIOD
+
+def ticks_diff(ticks1, ticks2):
+    "Compute the signed difference between two ticks values, assuming that they are within 2**28 ticks"
+    diff = (ticks1 - ticks2) & _TICKS_MAX
+    diff = ((diff + _TICKS_HALFPERIOD) & _TICKS_MAX) - _TICKS_HALFPERIOD
+    return diff
 
 # --------------------- step 1 - prepare eye bitmap translation matrix
         # 0 1 2 3 4 5 6 7 8  9 0 1 2 3 4 5 6 7 
@@ -92,7 +107,7 @@ radio_2.pull=digitalio.Pull.DOWN
 
 radio_3 = digitalio.DigitalInOut(board.D11) #d11 is B
 radio_3.direction = digitalio.Direction.INPUT
-#radio_3.pull=digitalio.Pull.DOWN
+radio_3.pull=digitalio.Pull.DOWN # TODO: verify if this was required or not
 
 radio_4 = digitalio.DigitalInOut(board.D10) # d10 is a
 radio_4.direction = digitalio.Direction.INPUT
@@ -108,7 +123,7 @@ pixels.show()
 displayio.release_displays()
 # --------------------- step 4 - prepare internal display
 
-
+# TODO: Get rid of all the extra stuff here
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
@@ -136,11 +151,27 @@ splash.append(inner_sprite)
 text = "Hello World!"
 text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=15)
 splash.append(text_area)
+# --------------------- prepare state machine
+
+state_display = 0 # 0-3 to match the RF token
+state_brightness = 0 # 0-3 for low/mid/high
 
 
-# --------------------- step 5 - enter loop 
+time_screentimeoutlength = 1000 # default length of mode text timeout on screen
+flag_update_text = True # preset to show initial start text
+flag_update_text_countingdown = True
+state_last_text = 0 
+time_screentimeout = ticks_add(supervisor.ticks_ms+time_screentimeoutlength)
+
+
+
+
+# --------------------- enter loop 
 
 while True:
+    time_clock = supervisor.ticks_ms()
+
+
     if radio_1.value==True:
         pixels[1]=(255,0,0)
     else:
