a2237d6a03b306d04966ec36983835f8c3e193dd
Daniel Garlans <dgarlans@gmail.com>
2023-07-03 00:30:39 -0400

    updated party mode significantly

--- code.py
+++ code.py
@@ -10,6 +10,9 @@ import displayio
 import digitalio
 import terminalio
 import neopixel
+import random
+import math
+import adafruit_fancyled
 from adafruit_display_text import label
 from bmp_reader import BMPReader
 
@@ -201,8 +204,9 @@ time_screentimeout = last_tick+time_screentimeoutlength
 
 # for reaction mode, 0 is neutral, and 1-2-3 are three different emotions. code is set to start as if the first button got clicked
 reaction_length=2500 
-reaction_mode = 4
+reaction_mode = 0
 reaction_timeout = last_tick + reaction_length
+
 
 partymode = False               # party mode is a toggle
 partybutton_inhibit = False     # this gets set when the party mode button is detected, and makes it so that the party mode won't toggle until the button has been let go
@@ -254,6 +258,7 @@ reaction_frame = 0 # a running counter, the update function does the math to pic
 reaction_nextframetime = 0 + reaction_defaultframetime # this get updated by the return from the update function1
 reaction_frame_needs_update = True # trigger to make the next image load
 
+next_blink = get_frametime() + 5000 # random.randint(500,3500)
 # TODO  reaction mode 4 is blink, add code to make end of reaction go to blink mode for a moment after a reaction times out - DONE
 #       however need to add an inhibit variable so that timeout for blink mode doesn't retrigger blink mode - DONE
 
@@ -265,13 +270,21 @@ def reactionframe(reactionid, framecounter):
     # TODO: add custom length and flow support
     frameid = framecounter % reaction[reactionid]
     #print("reaction frame " + str(frameid) + " this reaction has " + str(reaction[reactionid]) + " frames") 
-    loadreactionframe(reactionid, frameid+1) # +1 to handle the images being 1..x rather than 0..x, TODO maybe change this 
+    
+    if(reactionid==0):
+        randomframe = random.randint(1, reaction[reactionid])
+        print("random frame: " + str(randomframe))
+        loadreactionframe(reactionid, randomframe) # +1 to handle the images being 1..x rather than 0..x, TODO maybe change this 
+    else:
+        loadreactionframe(reactionid, frameid+1) # +1 to handle the images being 1..x rather than 0..x, TODO maybe change this 
 
     if(reactionid == 4):
         # blink mode
         return 750
+    if (reactionid ==1) or (reactionid == 2) or (reactionid == 3):
+        return 250
     else:
-        return reaction_defaultframetime # TODO: handle anything custom here
+        return random.randint(1000,4000) #reaction_defaultframetime # TODO: handle anything custom here
 
 # --------------------- enter loop 
 
@@ -286,7 +299,7 @@ while True:
         last_tick = current_tick
     if (current_tick >= reaction_nextframetime) and (reaction_frame_needs_update == False):
         reaction_frame += 1
-        #print("reaction frame update " + str(reaction_frame))
+        print("reaction frame update " + str(reaction_frame))
         reaction_frame_needs_update=True
 
 
@@ -309,28 +322,94 @@ while True:
     if radio_4.value==True:
         set_reaction(3)
 
-    if (last_reaction != reaction_mode): # this ensures that the new reaction gets presented immediately
-            last_reaction = reaction_mode
-            reaction_frame_needs_update=True
+    if(next_blink < current_tick):
+        if (partymode==True) or (reaction_mode==4): # if we're in party mode, or we're already blinking, defer the blink
+            next_blink=current_tick + random.randint(5000,10000)
+        else:
+            print("blink")
+            reaction_mode=4
+            reaction_timeout=current_tick+500
+            next_blink=current_tick + random.randint(5000,10000)
+
+    
     # handle reaction timeout
     if (reaction_timeout < current_tick) and (reaction_mode != 0): 
         print("reaction timed out " + str(reaction_mode))
         if (reaction_mode == 4): # blink mode times out to idle
             reaction_mode=0
+            #reaction_nextframetime=current_tick + random.randint(2000,4000)
+            #reaction_timeout = current_tick+random.randint(2000,4000) # this was required because blink return wasn't working
         else:
             reaction_mode=4 # all other reactions time out to blink
-            reaction_timeout = current_tick+750
+            reaction_timeout = current_tick+500
         reaction_frame_needs_update=True
     
         # TODO: send message to screen
-
+    if (last_reaction != reaction_mode): # this ensures that the new reaction gets presented immediately
+            last_reaction = reaction_mode
+            reaction_frame_needs_update=True
     # handle party mode
     # TODO: Change this to make party mode overwrite default idle mode but allow reactions to occur - DONE
     if(partymode and reaction_mode==0):
-        for y in range(0,12):
-            for x in range (0,18):
-                color = ((counter+x) % 18)* (255/18)
-                eyes[y][x] = (color, color, color )
+        party_submode = int((current_tick / 2000) % 4)
+        #print(party_submode)
+        #party_submode=4
+        if(party_submode == 0):
+            counter+=2
+            eye1=wheel(counter % 255)
+            eye2=wheel((counter + 128) % 255)
+            for y in range(0,12):
+                for x in range (0,9):
+                    eyes[y][x]=eye1
+            for y in range(0,12):
+                for x in range (9,18):
+                    eyes[y][x]=eye2
+        elif(party_submode ==1):
+            for y in range(0,12):
+                eyecolor = wheel(((counter)+(y*4)) % 255)
+                for x in range (0,18):
+                    eyes[y][x]=eyecolor
+        elif(party_submode ==2):
+            for y in range(0,12):
+                eyecolor = wheel(((counter)+(y*4)) % 255)
+                for x in range (0,18):
+                    eyes[11-y][x]=eyecolor
+        elif(party_submode ==3):
+            #print(math.sin(counter))
+            for y in range(0,12):
+                for x in range (0,18):
+                    hue = 4.0 + math.sin((x+counter/5.0) / 2.0) + math.sin(math.cos(((y+counter/2.4) / 2.3))) \
+				    + math.cos((x + y) / 3.2) + math.cos(counter/11.0)
+                    
+                    hue = hue * 32.0
+                    #print(hue)
+                    eyecolor =  wheel( int(hue))
+                    eyes[y][x]=eyecolor
+        elif(party_submode ==4):
+                
+            for y in range(0,12):
+                for x in range (0,18):
+                    hue = 1 + (math.sin((counter+y)/2.5)*math.cos(x/2))
+                    eyecolor=wheel(hue*127)
+                    eyes[y][x]=eyecolor
+
+
+        else:
+
+            for y in range(0,12):
+                for x in range (0,18):
+                    color = ((counter+x) % 18)* (255/18)
+                    if (party_submode==0):
+                        eyes[y][x] = (color, color, color)
+                    if (party_submode==1):
+                        eyes[y][x] = (color, 0, 0)
+                    if (party_submode==2):
+                        eyes[y][x] = (0, color, 0)
+                    if (party_submode==3):
+                        eyes[y][x] = (0, 0, color)
+                                            
+
+
         update_eyes(eyes, eyemap, pixels)
         pixels.show()
         counter+=1
